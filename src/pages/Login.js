d9750caeb1f62fd3d140ce17ee50761b53e0e636
Guilherme Viana <guilhermehviana01@gmail.com>
2021-09-07 12:34:05 -0300

    Alert test

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,8 +8,31 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
+      disabled: true,
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.validForm = this.validForm.bind(this);
+  }
+
+  validForm() {
+    const { password, email } = this.state;
+    const passwordLength = 6;
+
+    const errorCases = [
+      password.length < passwordLength,
+      !email.includes('@'),
+      email.split('@').length > 2,
+      !email.includes('.com'),
+    ];
+
+    const filledCorrect = errorCases.every((error) => error === false);
+
+    if (filledCorrect) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
   }
 
   handleChange({ target }) {
@@ -17,29 +40,45 @@ class Login extends React.Component {
     this.setState({
       [name]: value,
     });
+    // this.validForm();
+  }
+
+  // lert('Fire in the hole');
+
+  handleSubmit(event) {
+    const { email } = this.state;
+
+    const toMatch = email.indexOf('@');
+    console.log(toMatch);
+    const userName = `_${email[0].toUpperCase()}${email.substr(1, toMatch - 1)}_`;
+
+    alert(`Login Succesful! Your user name is  ${userName}`);
+    event.preventDefault();
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, disabled } = this.state;
     return (
       <div className="login-container">
         <img src={ LoginLogo } alt="Login logo" className="login-logo" />
-        <form>
+        <form onSubmit={ this.handleSubmit }>
           <input
             data-testid="email-input"
-            type="text"
+            type="email"
             name="email"
             value={ email }
             onChange={ this.handleChange }
+            onKeyUp={ this.validForm }
           />
           <input
             data-testid="password-input"
-            type="text"
+            type="password"
             name="password"
             value={ password }
             onChange={ this.handleChange }
+            onKeyUp={ this.validForm }
           />
-          <button type="submit">Entrar</button>
+          <button type="submit" disabled={ disabled }>Entrar</button>
         </form>
       </div>
     );
