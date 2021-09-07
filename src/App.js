228bb8738f659bbc42009049245d10d60836ff04
Guilherme Viana <guilhermehviana01@gmail.com>
2021-09-07 05:30:28 -0300

    Creates Login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   // Initing project!
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import '../Styles/Login.css';
+import LoginLogo from '../Login.png';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div className="login-container">
+        <img src={ LoginLogo } alt="Login logo" className="login-logo" />
+        <form>
+          <input
+            data-testid="email-input"
+            type="text"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <input
+            data-testid="password-input"
+            type="text"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
