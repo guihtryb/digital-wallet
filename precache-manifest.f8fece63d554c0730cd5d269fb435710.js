6ef382761203dfdd47a6afb26499ca7abaa6e48a
Guilherme Viana <guilhermehviana01@gmail.com>
2021-11-29 02:11:34 -0300

    Updates

---
+++ precache-manifest.f8fece63d554c0730cd5d269fb435710.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6fb101b0e83232a348a1a0f95e1a65",
+    "url": "/digital-wallet/index.html"
+  },
+  {
+    "revision": "a925fb463481bc81ba36",
+    "url": "/digital-wallet/static/css/main.92904a0a.chunk.css"
+  },
+  {
+    "revision": "19b8f88ba9207def17c6",
+    "url": "/digital-wallet/static/js/2.2900c882.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/digital-wallet/static/js/2.2900c882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a925fb463481bc81ba36",
+    "url": "/digital-wallet/static/js/main.c508ae8e.chunk.js"
+  },
+  {
+    "revision": "d3d8d90cdcb552c958ec",
+    "url": "/digital-wallet/static/js/runtime-main.6d29fa4e.js"
+  },
+  {
+    "revision": "259e22ce7a0b67baef329d2fc179c668",
+    "url": "/digital-wallet/static/media/LogoTest.259e22ce.png"
+  },
+  {
+    "revision": "6899dee4795dd9322a774fcd04e44b10",
+    "url": "/digital-wallet/static/media/headerLogo.6899dee4.jpg"
+  }
+]);
